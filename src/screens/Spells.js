72ec0eec2cf5e29a48a158ae82cf5d993708ebdd
Scott Johnson <Johnson.scott.14@gmail.com>
2019-03-27 13:54:48 -0600

    modified spellscreen page

--- src/screens/Spells.js
+++ src/screens/Spells.js
@@ -5,10 +5,35 @@ import {
     Text,
     TouchableOpacity,
     Image,
-    View
+    View,
+    Flatlist
 } from 'react-native';
+import HarryPotterService from '../services/harry.potter.service'
+import harryPotterService from '../services/harry.potter.service';
 
 export default class SpellsScreen extends Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: [],
+        }
+    }
+    componentWillMount() {
+        console.log('Two: componentWillMount');
+        this._getSpells();
+    }
+    _getSpells() {
+        harryPotterService.getSpells()
+        .then(results => {
+            this.setState({ data: results });
+        })
+        .catch(error => {
+            console.log('Something went wrong!');
+            console.log(error);
+        })
+    }
     render() {
         return (
             <View style={styles.container}>
@@ -17,10 +42,40 @@ export default class SpellsScreen extends Component {
                 style={{height: 70, alignItems: 'flex-start'}}
                 resizeMode="contain"
               />
-              <Text>Populate spells here</Text>
+                <View>
+                    {this.renderSpells()}
+                </View>
             </View>
         );
     }
+    renderSpells() {
+        return (
+            <FlatList
+                data={this.state.data}
+                keyExtractor={(item, index) => item.id.toString()}
+                renderItem={this._renderItem}
+                ListEmptyComponent={this._renderEmptyList}
+            />
+        );
+    }
+
+    _renderItem = ({ item }) => {
+        return (
+            <Text>
+            {item.getSpell()}
+            </Text>
+        );
+    }
+
+    _renderEmptyList = () => {
+        return (
+            <Text>...Just a few more seconds</Text>
+        );
+    }
+
+
+
+
 }
 
 const styles = StyleSheet.create({
